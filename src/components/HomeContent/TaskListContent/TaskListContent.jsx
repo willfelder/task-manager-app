99599fd0ff104029e6b04f3aa1daf053e7aec9d0
William Lengenfelder <william.lengenfelder@gmail.com>
2022-01-03 22:56:24 +0300

    update app

--- src/components/HomeContent/TaskListContent/TaskListContent.jsx
+++ src/components/HomeContent/TaskListContent/TaskListContent.jsx
@@ -1,4 +1,5 @@
 import React, {useContext} from 'react';
+import './tasklistcontent.scss';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 
@@ -7,9 +8,9 @@ const TaskListContent = ({duty}) => {
     return(
         <li className='list-items'>
             <span>{duty.duty}</span>
-            <div>
-                <button><DeleteIcon /></button>
-                <button><EditIcon /></button>
+            <div className='list-items_box'>
+                <div className='list-items_delete'><DeleteIcon /></div>
+                <div className='list-items_edit'><EditIcon /></div>
             </div>
         </li>
     )
