0d75428ece8970c6993c5d83c4edbbc83a524873
William Lengenfelder <william.lengenfelder@gmail.com>
2022-01-08 10:18:00 +0300

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './footer.scss';
+import {Link} from 'react-router-dom';
 
 const Footer = () => {
     return(
         <div className='footer_container'>
             <h3>Tasck was developed by William Lengenfelder</h3>
+                    <a href='https://github.com/willfelder' target='_blank'>
+                        <p>Visit my Github page </p>
+                    </a>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,18 @@ import Header from '../components/Header/Header';
 import HomeContent from '../components/HomeContent/HomeContent';
 import Footer from '../components/Footer/Footer';
 import TaskContextProvider from '../context/TaskContext';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const Home = () => {
     
     return(
-        <TaskContextProvider>
-            <Header />
-            <HomeContent />
-            <Footer />
-        </TaskContextProvider>
+        <Router>
+            <TaskContextProvider>
+                <Header />
+                <HomeContent />
+                <Footer />
+            </TaskContextProvider>
+        </Router>
     )
 }
 
