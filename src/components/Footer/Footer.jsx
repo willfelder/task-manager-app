932630a1b428039cf238aa811353f9916ca8b5f4
William Lengenfelder <william.lengenfelder@gmail.com>
2022-01-10 08:12:22 +0300

    deploy react app

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './footer.scss';
-import {Link} from 'react-router-dom';
 
 const Footer = () => {
     return(
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,15 @@ import Header from '../components/Header/Header';
 import HomeContent from '../components/HomeContent/HomeContent';
 import Footer from '../components/Footer/Footer';
 import TaskContextProvider from '../context/TaskContext';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 const Home = () => {
     
     return(
-        <Router>
             <TaskContextProvider>
                 <Header />
                 <HomeContent />
                 <Footer />
             </TaskContextProvider>
-        </Router>
     )
 }
 
